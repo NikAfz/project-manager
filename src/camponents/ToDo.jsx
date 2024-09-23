2773a4854449ef5ce0c3468660419d8b1916c381
NikAfz <nikafz980@gmail.com>
2024-09-23 19:08:28 +0330

    fix : blobs & ToDo

--- src/camponents/ToDo.jsx
+++ src/camponents/ToDo.jsx
@@ -2,110 +2,118 @@ import React from "react";
 import data from "../data";
 import { useState } from "react";
 
+import dh from "../assets/drag_handle.png";
+
 function ToDo(props) {
-  const [toDoInput, setToDoInput] = useState('')
+  const [toDoInput, setToDoInput] = useState("");
 
-  const [addToDoOpener, setAddToDoOpener] = useState(false)
+  const [addToDoOpener, setAddToDoOpener] = useState(false);
 
-
-  
   function handleSubmit(e) {
     e.preventDefault();
-    props.ToDo.push(toDoInput)
-    props.rander((p) => !p )
-    setToDoInput('')
-    setAddToDoOpener(false)
+    props.ToDo.push(toDoInput);
+    props.rander((p) => !p);
+    setToDoInput("");
+    setAddToDoOpener(false);
   }
-  
-  
+
   function handleAddingToDo() {
-    setAddToDoOpener(prev => !prev);
-  };
+    setAddToDoOpener((prev) => !prev);
+  }
 
   function handleSetNotDragging(insideOutside) {
     if (insideOutside === "inSide") {
-      props.setNotDragging(false)
+      props.setNotDragging(false);
     }
-    if(insideOutside !== "inSide"){
-      props.setNotDragging(true)
+    if (insideOutside !== "inSide") {
+      props.setNotDragging(true);
     }
-
   }
-  
-  
 
   const removeProjectList = () => {
-    
-    let Object = props.fullObject ;
+    let Object = props.fullObject;
     const i = data.indexOf(Object);
     if (i !== -1) {
       data.splice(i, 1);
     }
-    props.rander((p) => !p )
-
+    props.rander((p) => !p);
   };
 
   const removeToDo = (index) => {
-    
-    let ToDoArray = props.ToDo ;
+    let ToDoArray = props.ToDo;
     if (index !== -1) {
       ToDoArray.splice(index, 1);
     }
-    props.rander((p) => !p )
-
+    props.rander((p) => !p);
   };
-
-
 
   const renderToDo = props.ToDo.map((item, index) => {
     return (
-      <p className="todo" key={index} title="remove" onClick={() => removeToDo(index)}>
+      <p
+        className="todo"
+        key={index}
+        title="remove"
+        onClick={() => removeToDo(index)}
+      >
         {item}
       </p>
     );
   });
-  
 
-
-
-  return(
+  return (
     <>
-      <div className="dnd--div" 
-      onPointerOver={() => handleSetNotDragging("inSide")} 
-      onPointerOut={() => handleSetNotDragging("outSide")}
+      <div
+        className="dnd--div"
+        onPointerOver={() => handleSetNotDragging("inSide")}
+        onPointerOut={() => handleSetNotDragging("outSide")}
       >
-        <img src="src\assets\drag_handle_FILL0_wght400_GRAD0_opsz24.png" 
-          alt="drag here" 
-          width={35} 
-        />
+        <img src={dh} alt="drag here" width={35} />
       </div>
-      <div className="todo--list" onPointerOut={() => handleSetNotDragging("outSide")}>
-        
-        <div className="todo--list-header" >
-          <h2 className="todo--list-name" title={props.name} >{props.name}</h2>
-          <div className="remove" title="remove" onClick={() => removeProjectList()}>&#215;</div>
+      <div
+        className="todo--list"
+        onPointerOut={() => handleSetNotDragging("outSide")}
+      >
+        <div className="todo--list-header">
+          <h2 className="todo--list-name" title={props.name}>
+            {props.name}
+          </h2>
+          <div
+            className="remove"
+            title="remove"
+            onClick={() => removeProjectList()}
+          >
+            &#215;
+          </div>
         </div>
 
-        <div className="todo--list-content">
-          {renderToDo}
-          
-        </div>
+        <div className="todo--list-content">{renderToDo}</div>
         <div className="add-todo--container">
           {addToDoOpener ? (
             <form className="add-todo--form" onSubmit={handleSubmit}>
-              <input className="todo--inp" autoFocus placeholder="Add ToDo" value={toDoInput} onChange={e => setToDoInput(e.target.value)}/>
-              <button className="add-todo--btn" title="add" type="submit"  >&#10004;</button>
-            </form> 
+              <input
+                className="todo--inp"
+                autoFocus
+                placeholder="Add ToDo"
+                value={toDoInput}
+                onChange={(e) => setToDoInput(e.target.value)}
+              />
+              <button className="add-todo--btn" title="add" type="submit">
+                &#10004;
+              </button>
+            </form>
           ) : (
-            <button className="add-todo--btn" title="add todo" onClick={handleAddingToDo}>
+            <button
+              className="add-todo--btn"
+              title="add todo"
+              onClick={handleAddingToDo}
+            >
               &#43;
             </button>
           )}
         </div>
-
       </div>
     </>
-  )  
+  );
 }
 
 export default ToDo;
--- src/camponents/blobs.jsx
+++ src/camponents/blobs.jsx
@@ -1,45 +1,52 @@
 import React from "react";
+import b1 from "/blobanimation (4).svg";
+import b2 from "/blobanimation (5).svg";
+import b3 from "/blobanimation (6).svg";
+import b4 from "/blobanimation (7).svg";
+import b5 from "/blobanimation (14).svg";
+import b6 from "/blobanimation (13).svg";
+import b7 from "/blobanimation (11).svg";
+import b8 from "/blobanimation (8).svg";
+import b9 from "/blobanimation (9).svg";
+
 function Blobs() {
-  return(
+  return (
     <>
       <div className="b1">
-        <img className="bimg" src="../../public/blobanimation (4).svg" alt="" />
+        <img className="bimg" src={b1} alt="" />
       </div>
       <div className="b2">
-        <img className="bimg" src="../../public/blobanimation (5).svg" alt="" />
+        <img className="bimg" src={b2} alt="" />
       </div>
       <div className="b3">
-        <img className="bimg" src="../../public/blobanimation (5).svg" alt="" />
+        <img className="bimg" src={b2} alt="" />
       </div>
       <div className="b4">
-        <img className="bimg" src="../../public/blobanimation (6).svg" alt="" />
+        <img className="bimg" src={b3} alt="" />
       </div>
       <div className="b5">
-        <img className="bimg" src="../../public/blobanimation (7).svg" alt="" />
+        <img className="bimg" src={b4} alt="" />
       </div>
       <div className="b6">
-        <img className="bimg" src="../../public/blobanimation (14).svg" alt="" />
+        <img className="bimg" src={b5} alt="" />
       </div>
       <div className="b7">
-        <img className="bimg" src="../../public/blobanimation (13).svg" alt="" />
+        <img className="bimg" src={b6} alt="" />
       </div>
-
       <div className="bs1">
-        <img className="bimg" src="../../public/blobanimation (11).svg" alt="" />
+        <img className="bimg" src={b7} alt="" />
       </div>
       <div className="bs2">
-        <img className="bimg" src="../../public/blobanimation (8).svg" alt="" />
+        <img className="bimg" src={b8} alt="" />
       </div>
       <div className="bs3">
-        <img className="bimg" src="../../public/blobanimation (9).svg" alt="" />
+        <img className="bimg" src={b9} alt="" />
       </div>
       <div className="bs4">
-        <img className="bimg" src="../../public/blobanimation (14).svg" alt="" />
+        <img className="bimg" src={b5} alt="" />
       </div>
-
     </>
-
-  )
+  );
 }
 
 export default Blobs;
